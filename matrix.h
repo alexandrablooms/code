f650fab66124d7042771dc5fbd8f818750a8a631
alexandrablooms <gololobovaam@student.bmstu.ru>
2023-05-04 19:06:42 +0300

    Adding changes to project in Git

--- matrix.h
+++ matrix.h
@@ -15,10 +15,27 @@ namespace bmstu {
 
         Matrix(size_t rows, size_t columns) : data_(rows * columns), rows_(rows), columns_(columns) {
             for (size_t i = 0; i < rows_; ++i) {
-                std::vector<T *> current_row(columns_);
+                vector<T *> current_row(columns_);
                 for (size_t j = 0; j < columns_; ++j) {
                     current_row[j] = &(data_[i * columns_ + j]);
                 }
-                representation_.push_back(std::move(current_row));
+                representation_.push_back(move(current_row));
+            }
+        }
+
+        Matrix(initializer_list<T> i_list, size_t rows, size_t columns) : rows_(rows), columns_(columns) {
+            if (i_list.size() == rows_ * columns_) {
+                data_.resize(rows_ * columns_);
+                move(i_list.begin(), i_list.end(), data_.data());
+                for (size_t i = 0; i < rows_; ++i) {
+                    vector<T *> current_row(columns_);
+                    for (size_t j = 0; j < columns_; ++j) {
+                        current_row[j] = &(data_[i * columns_ + j]);
+                    }
+                    representation_.push_back(move(current_row));
+                }
+            } else {
+                throw out_of_range(
+                        "Current matrix position is wrong (" + to_string(rows) + ", " + to_string(columns) + ") != " + to_string(i_list.size()));
             }
         }
